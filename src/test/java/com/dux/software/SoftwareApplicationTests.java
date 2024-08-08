224e97f6b10ec025b23fee4ecfb19d0ac99fb460
francofallatti <francofallatti@gmail.com>
2024-08-08 20:57:08 -0300

    first commit

---
+++ src/test/java/com/dux/software/SoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dux.software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
